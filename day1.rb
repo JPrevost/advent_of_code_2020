414535181a2085aeccb522c23d72317dbbfc51ad
Jeremy Prevost <jprevost@mit.edu>
2020-12-01 13:30:31 -0500

    Refactor day 1 to permutations
    Why are these changes being introduced:
    
    * The brute force "just keep adding loops" approach was fine for 2 or 3
      addends, but even then it was a bit messy. Using permutations allows
      us to anticipate needing to extend this to support any number of
      addends in a logically easier to comprehend manner
    * adding a target value seems useful to avoid the magic number of "2020"
      that seems likely to change in future puzzles
    
    Thanks @gravesm for sharing his approach in python which allowed me to
    learn and modify my ruby approach.

--- day1.rb
+++ day1.rb
@@ -1,42 +1,12 @@
-report = File.open("input_day1.txt").readlines.map(&:chomp)
+report = File.open("input_day1.txt").readlines.map{|line| line.chomp.to_i}
 
-def expense_checker(expense_report, addends)
-
-  expense_report.each_with_index do |val,index|
-
-    expense_report.each_with_index do |val2, index2|
-      
-      expense_report.each_with_index do |val3, index3|
-        if val == val2 || val2 == val3 || val3 == val
-          # puts "same; skip"
-          next
-        else
-          if addends == 3
-            if (val.to_i + val2.to_i + val3.to_i) == 2020
-              puts "bingo: #{val} + #{val2} + #{val3}= #{val.to_i + val2.to_i + val3.to_i}"
-              puts "answer: #{val.to_i * val2.to_i * val3.to_i}"
-              exit
-            else
-              # puts "nope: #{val} + #{val2} = #{val.to_i + val2.to_i}"
-              next
-            end
-          elsif addends == 2
-            if (val.to_i + val2.to_i) == 2020
-              puts "bingo: #{val} + #{val2} = #{val.to_i + val2.to_i }"
-              puts "answer: #{val.to_i * val2.to_i}"
-              exit
-          else
-            # puts "nope: #{val} + #{val2} = #{val.to_i + val2.to_i}"
-            next
-            end
-          else
-            puts "unsupported number of addends"
-            return
-          end
-        end
-      end
+def expense_report(input, addends=3, target=2020)
+  input.permutation(addends).each do |p|
+    if p.reduce(:+) == target
+      return p.reduce(:*)
     end
   end
 end
 
-expense_checker(report, 3)
+puts expense_report(report, 2)
+puts expense_report(report, 3)
